826f08eed7ad4b61d4d9cbe1e0e50cee62bf0b55
Steve Geluso <stevegeluso@gmail.com>
2024-08-29 12:05:50 -0700

    eco

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,19 @@ export default function Home() {
     <div className="logo">
       <img src="./img/206_seattle_bike_polo_logo.png" />
     </div>
+    
+    <h1>Seattle Bike Polo</h1>
+    <h2>News</h2>
+    <ul>
+      <li>Come out October 26th and 27th 2024 for the <a
+          href="https://bikepolocalendar.com/event/the-cascadia-triple-crown-emerald-city-open">Emerald City Open: Cascadia Triple Crown</a>
+      </li>
+      <li>Tournament information <a href="/tournaments/2024-emerald-city-open-cascadia-triple-crown">here</a></li>
+      <li>Register for ECO <a href="https://forms.gle/6z6ba3aCkqtqJS416">here</a></li>
+      <li><a href="https://bikepolocalendar.com/event/the-cascadia-triple-crown-emerald-city-open">Bike Polo Calendar Page</a></li>
+    </ul>
+
+    <h2>Olds</h2>
     <div>
       <ul>
         <li>We play at Judkins Park <a href="https://maps.app.goo.gl/UxkWUMPTLsNPhg5WA">47.591401,-122.302915</a></li>
@@ -16,23 +29,24 @@ export default function Home() {
           <li>Sundays 11AM till dark</li>
           <li>Wednesdays 6PM till dark</li>
         </ul>
-        <li>Come out October 26th and 27th 2024 for the <a
-            href="https://bikepolocalendar.com/event/the-cascadia-triple-crown-emerald-city-open">Emerald City Open: Cascadia Triple Crown</a>
-            <ul><li>Register for ECO <a href="https://forms.gle/6z6ba3aCkqtqJS416">here</a></li></ul>
-        </li>
         <li><a href="https://www.instagram.com/206bikepolo/">@206bikepolo</a> on Instagram</li>
         <li>Come out June 1st and 2nd 2024 for the <a
             href="https://bikepolocalendar.com/event/nah-cascadia-qualifier">Cascadia Qualifier</a></li>
         <li><a href="https://www.instagram.com/206bikepolo/">@206bikepolo</a> on Instagram</li>
         <li>
-          Talk to everyone on
-          <a href="https://join.slack.com/t/seattlebikepolo/shared_invite/zt-2m8wd3zl2-RxZBe~fmA_1abfLNSAZ5yg">Slack</a>
+          Talk to everyone on <a href="https://join.slack.com/t/seattlebikepolo/shared_invite/zt-2m8wd3zl2-RxZBe~fmA_1abfLNSAZ5yg">Slack</a>
         </li>
         <li>
           Rules? Don't be a dick. Also <a href="/docs/2022-nah-ruleset.pdf">official NAH rules</a> and the <a href="/docs/polo_bible.pdf">polo bible</a>.
         </li>
         <li>Edit this website on <a href="">GitHub</a>. FTP and GitHub access avaialble upon request.</li>
       </ul>
+    </div>
+
+    <div>
+      <a href="http://localhost:3001/tournaments/2024-emerald-city-open-cascadia-triple-crown">
+        <img src="/img/2024_eco_3xcrown1.png" />
+      </a>
     </div>
 
     <Image src={judkinsPlayTimes} alt="Sign showing Judkins play times" />
