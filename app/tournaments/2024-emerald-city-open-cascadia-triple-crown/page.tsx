52dafeb36cc06e4a167dd47fb1b886b9b825ea0f
Steve Geluso <stevegeluso@gmail.com>
2024-08-29 11:59:50 -0700

    eco

--- app/tournaments/2024-emerald-city-open-cascadia-triple-crown/page.tsx
+++ app/tournaments/2024-emerald-city-open-cascadia-triple-crown/page.tsx
@@ -2,10 +2,26 @@ export default function Page() {
   return <div>
     <h1>2024 Emerald City Open: Cascadia Triple Crown</h1>
     <h2>
-      <a href="https://forms.gle/6z6ba3aCkqtqJS416">Registration is open.</a>
+      Registration is open.
     </h2>
+    <ul>
+      <li>
+        Click <a href="https://forms.gle/6z6ba3aCkqtqJS416">here</a> to register.
+      </li>
+      <li>
+        Or, click the image below.
+      </li>
+      <li>
+        Or, navigate your browser to this location https://forms.gle/6z6ba3aCkqtqJS416
+      </li>
+      <li>
+        Or, send news via carrier pigeon to the top of the Space Needle.
+      </li>
+    </ul>
     <div>
-      <img src="/img/2024_eco_3xcrown1.png" />
+      <a href="https://forms.gle/6z6ba3aCkqtqJS416">
+        <img src="/img/2024_eco_3xcrown1.png" />
+      </a>
     </div>
   </div>
 }
