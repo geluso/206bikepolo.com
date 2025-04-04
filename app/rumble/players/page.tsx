bce606a075e38eb8505d6f40bb68a9da37cda105
Steve Geluso <stevegeluso@gmail.com>
2025-04-04 01:56:21 -0700

    sort players by wrestler name on players page

--- app/rumble/players/page.tsx
+++ app/rumble/players/page.tsx
@@ -3,6 +3,7 @@ import PlayersTable from "./PlayersTable";
 
 export default async function Page() {
   const players = await usePlayers()
+  players.sort((p1, p2) => p1.wrestlerName.localeCompare(p2.wrestlerName))
   return <div>
     <p>Welcome to the Royal Rumble!!</p>
     <PlayersTable players={players} />
