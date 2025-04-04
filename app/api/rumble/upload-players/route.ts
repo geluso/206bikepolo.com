e8894238ed299f917eb7a9babd023f6ceb24e914
Steve Geluso <stevegeluso@gmail.com>
2025-04-04 02:57:02 -0700

    replace shitty csv with known good one

--- app/api/rumble/upload-players/route.ts
+++ app/api/rumble/upload-players/route.ts
@@ -1,64 +1,55 @@
 "use server"
 
 import { NextRequest, NextResponse } from 'next/server';
-import { parse } from 'csv-parse';
+import * as XLSX from 'xlsx';
 import prisma from '@/lib/prisma';
 import { setTournamentState } from '@/app/rumble/admin/manage-tournament-state/actions';
 
-type ResponseBody = { error: string } | { message: string };
-
 export async function POST(request: NextRequest) {
   const data = await request.formData();
-  return await new Promise<NextResponse<ResponseBody>>(async (resolve, reject) => {
-    const file = data.get('file') as File;
-    const tag = data.get('tag') as string;
-    console.log('Creating players with tag', tag)
+  const file = data.get('file') as File;
+  const tag = data.get('tag') as string;
+  console.log('Creating players with tag', tag)
 
-    if (!file) {
-      return resolve(NextResponse.json({ error: 'No file uploaded' }, { status: 400 }));
-    }
+  if (!file) {
+    return NextResponse.json({ error: 'No file uploaded' }, { status: 400 });
+  }
 
-    const playerWithTag = await prisma.royalRumblePlayer.findFirst({ where: { tag }})
-    if (playerWithTag) {
-      return resolve(NextResponse.json({ error: `Tag '${tag}' already exists. Choose a unique tag.` }, { status: 400 }));
-    }
+  const playerWithTag = await prisma.royalRumblePlayer.findFirst({ where: { tag }})
+  if (playerWithTag) {
+    return NextResponse.json({ error: `Tag '${tag}' already exists. Choose a unique tag.` }, { status: 400 });
+  }
 
-    const text = await file.text();
-    const records: {tag: string, spreadsheetPlayerId: string, player: string, wrestlerName: string }[] = [];
+  type JSONRow = { 'PID': string, 'PLAYER': string, 'WRESTLER NAME': string }
 
-    parse(text, {
-      columns: true,
-      skip_empty_lines: true
-    }, async (err, output) => {
-      if (err) {
-        return resolve(NextResponse.json({ error: 'Error parsing CSV' }, { status: 500 }));
-      }
+  const buffer = await file.arrayBuffer()
+  const workbook = XLSX.read(buffer);
+  const sheet = workbook.Sheets[workbook.SheetNames[0]];
+  const json: JSONRow[] = XLSX.utils.sheet_to_json(sheet)
 
-      for (const record of output) {
-        const spreadsheetPlayerId = record['PID'].trim()
-        const player = record['PLAYER'].trim()
-        const wrestlerName = record['WRESTLER NAME'].trim()
-        records.push({
-          tag,
-          spreadsheetPlayerId,
-          player,
-          wrestlerName,
-        });
-      }
-      try {
-        const created = await prisma.royalRumblePlayer.createMany({
-          data: records,
-          skipDuplicates: true
-        });
-        console.log({created})
-
-        await setTournamentState(tag, 'round1')
-
-        resolve(NextResponse.json({ message: `${records.length} players created successfully.` }, { status: 200 }))
-      } catch (error) {
-        console.error('upload-players', error)
-        resolve(NextResponse.json({ error: `Error creating players.` }, { status: 500 }));
-      }
+  const records: {tag: string, spreadsheetPlayerId: string, player: string, wrestlerName: string }[] = [];
+  json.forEach(row => {
+    const spreadsheetPlayerId = row['PID'].trim()
+    const player = row['PLAYER'].trim()
+    const wrestlerName = row['WRESTLER NAME'].trim()
+    records.push({
+      tag,
+      spreadsheetPlayerId,
+      player,
+      wrestlerName,
     });
   })
+  try {
+    const created = await prisma.royalRumblePlayer.createMany({
+      data: records,
+      skipDuplicates: true
+    });
+
+    await setTournamentState(tag, 'round1')
+
+    return NextResponse.json({ message: `${records.length} players created successfully.` }, { status: 200 })
+  } catch (error) {
+    console.error('upload-players', error)
+    return NextResponse.json({ error: `Error creating players.` }, { status: 500 });
+  }
 }
