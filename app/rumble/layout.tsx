37ca7193d23311eb1d0951db5680a62178991c13
Steve Geluso <stevegeluso@gmail.com>
2025-04-04 15:19:34 -0700

    toggleable navbar

--- app/rumble/layout.tsx
+++ app/rumble/layout.tsx
@@ -1,16 +1,18 @@
 import Link from "next/dist/client/link";
+import { getCurrentTagSettings } from "./util/getTags";
 
-export default function Page({ children }: { children: React.ReactNode }) {
+export default async function Page({ children }: { children: React.ReactNode }) {
+  const tagSettings = await getCurrentTagSettings()
   return <div>
-    <h1>🌹☠️ 2025 Rose City Royal Rumble 💀🥀</h1>
+    <h1>🌹☠️ 2025 Rose City Royal Rumble <Link className="no-underline" href="/rumble/admin">💀</Link>🥀</h1>
     <div>
       <Link href="/rumble/players">Players</Link> |{' '}
-      <Link href="/rumble/rounds/1">Round 1</Link> |{' '}
-      <Link href="/rumble/rounds/2">Round 2</Link> |{' '}
-      <Link href="/rumble/rounds/3">Round 3</Link> |{' '}
-      <Link href="/rumble/rounds/4">Round 4</Link> |{' '}
-      <Link href="/rumble/rounds/5">Round 5</Link> |{' '}
-      <Link href="/rumble/rounds/finals">Final Teams</Link> |{' '}
+      {tagSettings?.isRound1Up && <><Link href="/rumble/rounds/1">Round 1</Link> |{' '}</>}
+      {tagSettings?.isRound2Up && <><Link href="/rumble/rounds/2">Round 2</Link> |{' '}</>}
+      {tagSettings?.isRound3Up && <><Link href="/rumble/rounds/3">Round 3</Link> |{' '}</>}
+      {tagSettings?.isRound4Up && <><Link href="/rumble/rounds/4">Round 4</Link> |{' '}</>}
+      {tagSettings?.isRound5Up && <><Link href="/rumble/rounds/5">Round 5</Link> |{' '}</>}
+      {tagSettings?.isFinalTeamsUp && <><Link href="/rumble/rounds/finals">Final Teams</Link> |{' '}</>}
       <Link href="/rumble/bracket">Bracket</Link>
     </div>
     <div>
