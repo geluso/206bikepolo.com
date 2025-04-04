c756e9124954d6012df0aae81ecdaa10891ef4df
Steve Geluso <stevegeluso@gmail.com>
2025-04-04 05:19:59 -0700

    view playeys

--- app/rumble/page.tsx
+++ app/rumble/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
-import judkinsPoloHistory from '../../public/img/judkins_polo_history.jpg'
+import Link from "next/link";
 
 export default function Page() {
   return <div>
     <p>Welcome to the Royal Rumble!!</p>
+    <Link href="/rumble/players">View players</Link>
   </div>
 }
