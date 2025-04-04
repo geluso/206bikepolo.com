0f864184f7b4c322aa4605f03071cbb5bdfbeaf2
Steve Geluso <stevegeluso@gmail.com>
2025-04-04 15:31:16 -0700

    1/mixB/60 team algorithm

--- app/rumble/admin/create-teams/actions.ts
+++ app/rumble/admin/create-teams/actions.ts
@@ -3,11 +3,22 @@
 import prisma from "@/lib/prisma";
 import { PlayerStandings } from "../../util/getPlayerStandings";
 
+function shuffle(array: any[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
 export async function createTeams(tag: string, series: string, standings: PlayerStandings) {
+  console.log('createTeams', tag, series)
   const isAlreadyCreated = await prisma.royalRumbleTeam.findFirst({ where: { tag, series }})
   if (isAlreadyCreated) {
     throw `Error createTeams already created [tag=${tag}, series=${series}]` 
   }
+
+  const bPlayers = standings.sortedPlayers.slice(20, 40)
+  shuffle(bPlayers)
 
   const sortedTeams = []
   for (let i = 0; i < 20; i++) {
@@ -18,8 +29,8 @@ export async function createTeams(tag: string, series: string, standings: Player
     }
     const name = 'Team ' + twoDigitTeamNumber
     const player1Id = standings.sortedPlayers[i].id
-    const player2Id = standings.sortedPlayers[i + 20].id
-    const player3Id = standings.sortedPlayers[i + 40].id
+    const player2Id = bPlayers[i].id
+    const player3Id = standings.sortedPlayers[standings.sortedPlayers.length - 1 - i].id
     const team = {
       name,
       tag,
@@ -29,7 +40,6 @@ export async function createTeams(tag: string, series: string, standings: Player
       player3Id
     }
     sortedTeams.push(team)
-    console.log('create', team)
   }
 
   const createdTeams = await prisma.royalRumbleTeam.createManyAndReturn({ data: sortedTeams, skipDuplicates: true })
