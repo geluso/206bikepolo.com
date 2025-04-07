cc8bc3faff9dbeaaadbd79809502ee8992b72d09
Steve Geluso <stevegeluso@gmail.com>
2025-04-06 18:36:22 -0700

    game 35 udpate

--- app/rumble/bracket/Bracket.tsx
+++ app/rumble/bracket/Bracket.tsx
@@ -113,10 +113,10 @@ export function Bracket() {
                 </div>
                 {/* <!-- Round 5 --> */}
                 <div className="round round-5">
-                    <div className="match game-current">
+                    <div className="match game-complete">
                         <div className="game-title">Game 35</div>
-                        <div className="team">Gabe/Aaron/Non<input className="input-field" value={" "}/></div>
-                        <div className="team">OG Hard Back Julian/LaRosa/Squigg<input className="input-field" value={" "}/></div>
+                        <div className="team">Gabe/Aaron/Non<input className="input-field" value={"4"}/></div>
+                        <div className="team">OG Hard Back Julian/LaRosa/Squigg<input className="input-field" value={"3"}/></div>
                     </div>
                 </div>
                 
@@ -124,7 +124,7 @@ export function Bracket() {
                 <div className="round final-round">
                     <div className="match game-staged">
                         <div className="game-title">Final</div>
-                        <div className="team">Winner G35 <input className="input-field" value={" "}/></div>
+                        <div className="team">Gabe/Aaron/Non <input className="input-field" value={" "}/></div>
                         <div className="team">Winner G37 <input className="input-field" value={" "}/></div>
                     </div>
                 </div>
@@ -247,7 +247,7 @@ export function Bracket() {
                 {/* <!-- Round 6 --> */}
                 <div className="round round-6">
                     <div className="round-title">Round 6</div>
-                    <div className="match game-staged">
+                    <div className="match game-current">
                         <div className="game-title">Game 36</div>
                         <div className="team">Weston/Cap/Kai<input className="input-field" value={" "}/></div>
                         <div className="team">Los Lobos (TimTim/Andreas/Mayo) <input className="input-field" value={" "}/></div>
@@ -259,7 +259,7 @@ export function Bracket() {
                     <div className="round-title">Round 7</div>
                     <div className="match game-staged">
                         <div className="game-title">Game 37</div>
-                        <div className="team">Loser G35 <input className="input-field" value={" "}/></div>
+                        <div className="team">OG Hard Back Julian/LaRosa/Squigg <input className="input-field" value={" "}/></div>
                         <div className="team">Winner G36 <input className="input-field" value={" "}/></div>
                     </div>
                 </div>
