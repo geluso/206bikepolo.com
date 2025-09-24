5cf33f1a89dbeabc052a50b01f95bdeb823c6c3e
Abhay Gupta <ab18gu@gmail.com>
2025-09-24 13:23:31 -0700

    Update polo tourney page with  links (hyperlinks broken, generated via claude)

--- app/tournaments/page.tsx
+++ app/tournaments/page.tsx
@@ -1,12 +1,147 @@
 export default function Page() {
-  return <div>
-    <ul>
-      <li>
-        <a href="/tournaments/2024-emerald-city-open-cascadia-triple-crown">2024 Emerald City Open: Cascadia Triple Crown</a>
-      </li>
-      <li>
-        <a href="/tournaments/2024-cascadia-qualifier">2024 Cascadia Qualifier</a>
-      </li>
+  return <div className="mb-80">
+
+  <h2 className="my-2 text-2xl font-extrabold">Tournaments</h2>
+  <p className="mb-2">
+    The bike polo tournaments around the world are listed here:{" "}
+    <a href="https://bikepolocalendar.com/" target="_blank">
+      https://bikepolocalendar.com/
+    </a>
+  </p>
+
+  <h2 className="my-2 text-2xl font-extrabold">Major Upcoming 2025 Tournaments</h2>
+  <ul className="ps-6 my-2 list-disc list-inside">
+    <li><a href="https://www.facebook.com/events/1238786489274746" target="_blank">
+        2025 North American Hardcourt Championships (NAHC) - August 1-3, 2025 in Chicago, IL
+      </a></li>
+    <li><a href="https://www.bikepoloworldcup.com/" target="_blank">
+        2025 World Cup - July 11-13, 2025 in Munich, Germany
+      </a></li>
+    <li><a href="https://www.facebook.com/events/380772648646648" target="_blank">
+        2025 European Hardcourt Championships (EHC) - June 27-29, 2025 in Warsaw, Poland
+      </a></li>
+    <li><a href="https://www.facebook.com/events/703823682195579" target="_blank">
+        2025 Asian Hardcourt Championships (AHC) - Date and location TBA
+      </a></li>
+    <li><a href="https://www.facebook .com/events/whbpc-2025/whbpc-2025/230202919057524" target="_blank">
+        2025 World Hardcourt Bike Polo Championships (WHBPC) - November 13-16, 2025 in Taiwan
+      </a></li>
+  </ul>
+
+  <h2 className="my-2 text-2xl font-extrabold">Local</h2>
+  <p className="mb-2">
+    Common tournaments that Seattlites attend:
+  </p>
+
+  <ul className="ps-6 my-2 list-disc list-inside">
+    <li>
+      <a href="https://www.facebook.com/events/1238786489274746" target="_blank">
+        Bellingham Halloween
+      </a>{" "}
+      - Usually in October in Bellingham, WA
+    </li>
+    <li>
+      <a href="https://bikepolocalendar.com/event/portland-open" target="_blank">
+        Portland Open
+      </a>{" "}
+      - Usually in October in Portland, OR
+    </li>
+    <li>
+      <a href="https://bikepolocalendar.com/event/bellingham-open" target="_blank">
+        Bellingham Open
+      </a>{" "}
+      - Usually in September in Bellingham, WA
+    </li>
+    <li>
+      <a href="https://bikepolocalendar.com/event/soft-serve-swirl-iv" target="_blank">
+        Soft Serve Swirl
+      </a>{" "}
+      - Usually in October in Vancouver, BC
+    </li>
+    <li>
+      <a href="https://bikepolocalendar.com/event/no-fun-city-9" target="_blank">
+        No Fun City
+      </a>{" "}
+      - Usually in September in Vancouver, BC
+    </li>
+    <li>
+      <a href="https://bikepolocalendar.com/event/rose-city-royal-rumble" target="_blank">
+        Rose City Royal Rumble
+      </a>{" "}
+      - Usually in April in Portland, OR
+    </li> 
+  </ul>
+
+  <strong>Seattle Bike Polo hosts</strong> several tournaments each year. Here are some from previous years:
+
+    <ul className="ps-6 my-2 list-disc list-inside">
+      <li><a href="/tournaments/2024-emerald-city-open-cascadia-triple-crown">
+            2024 Emerald City Open: Cascadia Triple Crown
+          </a></li>
+      <li><a href="/tournaments/2024-cascadia-qualifier">
+            2024 Cascadia Qualifier
+          </a></li>
     </ul>
+
+
+  <h2 className="my-2 text-2xl font-extrabold">Nearby Clubs</h2>
+  <ul className="ps-6 my-2 list-disc list-inside">
+    <li><a href="https://portlandbikepolo.com/" target="_blank">
+        Portland Bike Polo
+      </a></li>
+    <li><a href="https://www.facebook.com/groups/boisehardcourtbikepolo" target="_blank">
+        Boise Bike Polo
+      </a></li>
+    <li><a href="https://www.facebook.com/groups/SpokaneBikePolo" target="_blank">
+        Spokane Bike Polo
+      </a></li>
+    <li><a href="https://www.facebook.com/groups/bikepolocanada" target="_blank">
+        Vancouver BC Bike Polo
+      </a></li>
+  </ul>
+
+  <h2 className="my-2 text-2xl font-extrabold">Popular US Clubs</h2>
+  <ul className="ps-6 my-2 list-disc list-inside">
+    <li><a href="https://www.nycbikepolo.com/" target="_blank">
+        NYC Bike Polo
+      </a></li>
+    <li><a href="https://www.chicagobikepolo.com/" target="_blank">
+        Chicago Bike Polo
+      </a></li>
+    <li><a href="https://www.facebook.com/PhillyBikePolo/" target="_blank">
+        Philly Bike Polo
+      </a></li>
+    <li><a href="https://www.facebook.com/groups/atlbikepolo" target="_blank">
+        Atlanta Bike Polo
+      </a></li>
+    <li><a href="https://www.facebook.com/groups/bikepolosf" target="_blank">
+        San Francisco Bike Polo
+      </a></li>
+    <li><a href="https://www.facebook.com/groups/SeattleBikePolo/" target="_blank">
+        Seattle Bike Polo
+      </a></li>
+  </ul> 
+
+  <h2 className="my-2 text-2xl font-extrabold">Popular Global Clubs</h2>
+  <ul className="ps-6 my-2 list-disc list-inside">
+    <li><a href="https://www.bikepolointernational.com/" target="_blank">
+        Bike Polo International
+      </a></li>
+    <li><a href="https://www.facebook.com/groups/WorldBikePolo/" target="_blank">
+        World Bike Polo
+      </a></li>
+  </ul>
+
+  <h2 className="my-2 text-2xl font-extrabold">Links</h2>
+  <ul className="ps-6 my-2 list-disc list-inside">
+    <li><a href="https://en.wikipedia.org/wiki/Hardcourt_Bike_Polo" target="_blank">
+        Wikipedia page
+      </a> on Hardcourt Bike Polo</li>
+    <li><a href="https://www.reddit.com/r/BIKEPOLO/" target="_blank">
+        Reddit page
+      </a> on Bike Polo</li>
+  </ul>
+
+  <p className="pb-10"></p>
   </div>
 }
