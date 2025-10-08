62eb5ae736062ae003957852a90f14071aaff4a5
Abhay Gupta <ab18gu@gmail.com>
2025-10-08 03:36:20 -0700

    Update royale rumble page

--- app/rumble/layout.tsx
+++ app/rumble/layout.tsx
@@ -3,13 +3,14 @@ import { getCurrentTagSettings } from "./util/getTags";
 
 export default async function Page({ children }: { children: React.ReactNode }) {
   const tagSettings = await getCurrentTagSettings()
-  return <div>
-    <h1>🌹☠️ 2025 Rose City Royal Rumble <Link className="no-underline" href="/rumble/admin">💀</Link>🥀</h1>
+  return (
     <div>
-      <Link href="/rumble/players">Players</Link> |{' '}
-      {tagSettings?.isRound1Up && <><Link href="/rumble/rounds/1">Round 1</Link> |{' '}</>}
-      {tagSettings?.isRound2Up && <><Link href="/rumble/rounds/2">Round 2</Link> |{' '}</>}
-      {tagSettings?.isRound3Up && <><Link href="/rumble/rounds/3">Round 3</Link> |{' '}</>}
+      <h2 className=" my-2 text-2xl font-extrabold">🌹☠️ 2025 Rose City Royal Rumble <Link className="no-underline" href="/rumble/admin">💀</Link>🥀</h2>
+      <div className="my-2">
+        <Link href="/rumble/players">Players</Link> |{' '}
+        {tagSettings?.isRound1Up && <><Link href="/rumble/rounds/1">Round 1</Link> |{' '}</>}
+        {tagSettings?.isRound2Up && <><Link href="/rumble/rounds/2">Round 2</Link> |{' '}</>}
+        {tagSettings?.isRound3Up && <><Link href="/rumble/rounds/3">Round 3</Link> |{' '}</>}
       {tagSettings?.isRound4Up && <><Link href="/rumble/rounds/4">Round 4</Link> |{' '}</>}
       {tagSettings?.isRound5Up && <><Link href="/rumble/rounds/5">Round 5</Link> |{' '}</>}
       <Link href="/rumble/standings">Standings</Link> |{' '}
