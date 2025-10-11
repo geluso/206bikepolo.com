0364b1464606e69b76f89bdfd51000c9a021ba94
Abhay Gupta <ab18gu@gmail.com>
2025-10-11 10:33:55 -0700

    Add logo to corner (white)

--- app/components/NavigationTabs.tsx
+++ app/components/NavigationTabs.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import NavItem from "./NavItem";
 import HamburgerMenu from "./HamburgerMenu";
 
+import Image from "next/image";
+import seattleWhitePoloLogo from '../../public/img/206_white_seattle_polo_logo.png'
+
 interface Tab {
   label: string;
   href: string;
@@ -17,8 +20,14 @@ const NavigationTabs: React.FC<NavigationTabsProps> = ({ tabs }) => {
     <nav className="bg-gray-900 sticky w-full z-20 top-0 start-0">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3">
+            <Image 
+              src={seattleWhitePoloLogo} 
+              alt="Judkins Park Bike Polo history community meeting poster timeline"
+              width={70}
+              height={70}
+            /> 
           <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
-            Seattle Bike Polo
+            Bike Polo
           </span>
         </a>
         <HamburgerMenu tabs={tabs}/>
