f12536691f34f9d2c5ca24bbab0d53f9f172ffb8
Abhay Gupta <ab18gu@gmail.com>
2025-10-27 02:28:55 -0700

    Add empty photos page

---
+++ app/photos/page.tsx
@@ -0,0 +1,17 @@
+export default function Page() {
+    return (
+              <div className="mb-32">
+                <p>
+                    TODO: Render images from: <a href="https://www.instagram.com/206bikepolo/">https://www.instagram.com/206bikepolo/</a>
+                </p>
+                <ul className="ps-6 my-2 list-disc list-inside">
+                    <li>To fetch the images from instagram, you have to produce a id/token via:</li>
+                        <ul className="ps-6 list-disc list-inside">
+                            <li>this "Facebook Developer App" feature within ig/facebook. </li>
+                        </ul>
+                    <li>But I do not have ig login... so waiting for someone to help out :(.</li>
+                </ul>
+
+              </div>
+    )
+};
